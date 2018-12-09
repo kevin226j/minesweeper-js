9564f9572c19fc3fe750f1bed5f6ad93b1f2880d
kevin226j <kevin226j@gmail.com>
2018-12-09 13:45:20 -0800

    build board function created

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # minesweeper-js
-Minesweeper game built with pure JS
+Minesweeper game built with pure JS. 
+
+Original insipration came from Cody Seibert, from his his youtube [video](https://www.youtube.com/watch?v=iscLNDuFLHU)
+
+Decided to implement same project using pure javascript, to take advantage of native language capabilities and to learn how and why javascript works.
+
+Enjoy!
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,38 @@
-console.log('hello world!')
+//************************************************************************/
+//SET ELEMENTS FOR BOARD:
+//************************************************************************/
+const BOARD = document.getElementById('board');
+const ROWS = COLS = 10;
+
+
+//************************************************************************/
+//BUILD BOARD:
+//************************************************************************/
+(function createBoard() {
+    for(let i = 0; i < ROWS; i++){
+        let row = document.createElement('div');
+        row.setAttribute('class', 'row');
+
+        for(let j = 0; j < COLS; j++){
+            let col = document.createElement('div');
+            col.setAttribute('data-row', `${i}`);
+            col.setAttribute('data-col', `${j}`);
+            col.classList += 'col hidden ';
+            if(Math.random() < 0.2) col.classList += 'mine';
+
+            row.appendChild(col);
+        }
+        BOARD.appendChild(row);
+    }
+})()
+
+
+
+
+//************************************************************************/
+//CLICK:
+//************************************************************************/
+BOARD.addEventListener('click', function(e) {
+    const target = e.target.classList;
+    console.log(target);
+})

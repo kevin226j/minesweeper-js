4fd19ece98909700541e657e55acdf3ea6296ee0
kevin226j <kevin226j@gmail.com>
2018-12-09 15:31:19 -0800

    implemented reveal and mine count function

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,16 +9,20 @@ const ROWS = COLS = 10;
 //BUILD BOARD:
 //************************************************************************/
 (function createBoard() {
-    for(let i = 0; i < ROWS; i++){
+    for (let i = 0; i < ROWS; i++) {
+        //create row with new div element
         let row = document.createElement('div');
         row.setAttribute('class', 'row');
 
-        for(let j = 0; j < COLS; j++){
+        for (let j = 0; j < COLS; j++) {
+            //create col with new div element
             let col = document.createElement('div');
             col.setAttribute('data-row', `${i}`);
             col.setAttribute('data-col', `${j}`);
             col.classList += 'col hidden ';
-            if(Math.random() < 0.2) col.classList += 'mine';
+
+            //randomize mines on board
+            if (Math.random() < 0.1) col.classList += 'mine';
 
             row.appendChild(col);
         }
@@ -27,12 +31,103 @@ const ROWS = COLS = 10;
 })()
 
 
+//************************************************************************/
+//GAME OVER:
+//************************************************************************/
+function gameOver(val) {
+    console.log('Game Over!');
+}
+
+
+//************************************************************************/
+//REVEAL:
+//************************************************************************/
+function reveal(row, col) {
+    let hasSeen = {};
+
+    function search(i, j) {
+        if (i >= ROWS || j >= COLS || i < 0 || j < 0) return;
+        const key = `${i}${j}`
+
+        //check to see if cell has been seen, if not run commands
+        if (hasSeen[key]) {
+            return
+        } else {
+            hasSeen[key] = true;
+            let cell = document.querySelector(`.col.hidden[data-row="${i}"][data-col="${j}"]`);
+            if (!cell) return;
+
+            //Get surrounding mine count
+            let mineCount = getMineCount(i, j);
+
+            if (!cell.classList.contains('hidden') || cell.classList.contains('mine')) return;
+            cell.classList.remove('hidden')
+
+            //if count exists, write to cell
+            if (mineCount) {
+                cell.innerHTML = mineCount;
+                return;
+            }
+
+            // Implement recursive FloodFill to check surroundings
+            for (let ii = -1; ii <= 1; ii++) {
+                for (let jj = -1; jj <= 1; jj++) {
+                    search(i + ii, j + jj);
+                }
+            }
+        }
+    }
+    //cast to number and call search function
+    search(+row, +col)
+}
+
+//************************************************************************/
+//GET MINE COUNT:
+//************************************************************************/
+function getMineCount(i, j) {
+    let count = 0;
+
+    // Implement recursive FloodFill to check surroundings
+    for (let ii = -1; ii <= 1; ii++) {
+        for (let jj = -1; jj <= 1; jj++) {
+            const ni = i + ii;
+            const nj = j + jj;
+
+            if (ni >= ROWS || nj >= COLS || nj < 0 || ni < 0) continue;
+            const cell = document.querySelector(`.col.hidden[data-row="${ni}"][data-col="${nj}"]`)
+
+            if (!cell) continue;
+
+            //If class contains 'mine', increase the count
+            if (cell.classList.contains('mine')) count++;
+        }
+    }
+    return count;
+}
 
 
 //************************************************************************/
 //CLICK:
 //************************************************************************/
-BOARD.addEventListener('click', function(e) {
-    const target = e.target.classList;
-    console.log(target);
-})
+BOARD.addEventListener('click', function (e) {
+    const targetClass = e.target.classList;
+    if (targetClass.contains('hidden') && targetClass.contains('col')) {
+        if (targetClass.contains('mine')) {
+            gameOver(false)
+        } else {
+            const cell = e.target
+            const row = cell.dataset.row;
+            const col = cell.dataset.col;
+            //Reveal function
+            reveal(row, col);
+
+            //Collect remaining hidden and mine elements for comparison
+            const collectHiddenElems = document.querySelectorAll('.col.hidden');
+            const collectMineElems = document.querySelectorAll('.col.mine');
+            const isGameOver = collectHiddenElems.length === collectMineElems.length;
+            if (isGameOver){
+                gameOver(true);
+            }
+        }
+    }
+}, false);
